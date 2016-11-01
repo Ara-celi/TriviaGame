91603b1310daa78a9f1475a4105ea778995da450
Guillermo <Guillermo@Guillermos-MacBook-Pro.local>
2016-10-31 23:58:19 -0400

    begin js

---
+++ assets/javascript/app.js
@@ -0,0 +1,95 @@
+var correctAnswers=0
+var incorrectAnswers=0
+var unanswered=0
+var reset;
+
+var questions=[
+  "What year was the first 'MTV Music Awards' hosted?",
+  "Johnny Depp’s first big role was in?",
+  "Which actor was NOT part of 'The Breakfast Club'?",
+  "Molly Ringwalds big-screen debut was in the film?",
+  "Which artist did not participate in the recording of the song, 'We are the World’?",
+  "Which 80’s TV series became an animated show?",
+  "What was the highest-grossing film in the 80’s?",
+  "Which animated cartoon had the character Penny?",
+];
+var choices0=['1981', '1984', '1985', '1988'];
+var choices1=['Edward Scissorhands', '21 Jump Street', 'A nightmare on Elm St.', 'Platoon'];
+var choices2=['Paul Gleason', 'Charlie Sheen', 'Anthony Micheal Hall', 'Emilio Estevez'];
+var choices3=['Sixteen Candles', 'The Breakfast Club', 'Pretty in Pink','Tempest'];
+var choices4=['Prince', 'Lionel Richie', 'Michael Jackson', 'Huey Lewis'];
+var choices5=['Small Wonder', 'Punky  Brewster', 'Greatest American Hero', 'Saved by the Bell'];
+var choices6=['E.T. the Extra-Terrestrial', 'GhostBusters', 'Jaws', 'Starwars Return of the Jedi'];
+var choices7=['JEM', 'Inspector Gadget', 'Thundercats', 'The Littles'];
+
+var answers=[
+'1984',
+'A nightmare on Elm St.',
+'Charlie Sheen',
+'Tempest',
+'Prince',
+'Punky  Brewster',
+'E.T. the Extra-Terrestrial',
+'Inspector Gadget'
+];
+
+console.log("Start TriviaGame here ");
+
+
+function playGame(){
+  for (var i = 0; i < questions.length; i++){
+    document.getElementById
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
